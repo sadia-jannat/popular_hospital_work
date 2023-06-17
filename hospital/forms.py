0dac77234812b6c8806eda3cdb242b06f6829e8d
sadia-jannat <37320382+sadia-jannat@users.noreply.github.com>
2023-06-17 12:31:46 +0600

    Update forms.py

--- hospital/forms.py
+++ hospital/forms.py
@@ -31,4 +31,21 @@ class DoctorDetailsForm(forms.ModelForm):
 class SearchForm(forms.Form):
     query = forms.CharField(max_length=100)   
     #querycategory=forms.CharField(max_length=100)
-    #queryday=forms.CharField(max_length=100)     
+    #queryday=forms.CharField(max_length=100) 
+
+class PatientForm(forms.ModelForm):
+    class Meta:
+        model=Patient
+        fields=['doctor_name','name', 'phone', 'email', 'location', 'patient_category',  'details']
+        labels={'doctor_name':'Enter doctor name', 'name':'Enter your name', 'phone':'Enter your phone','email':'Enter you email',
+                'location':'Your location', 'patient_category':'Are you New patient?',
+                'details':'Enter your some details of diseases history'}
+        widgets={
+            'doctor_name':forms.TextInput(attrs={'class':'form-control'}),
+            'name': forms.TextInput(attrs={'class':'form-control'}),
+            'phone': forms.NumberInput(attrs={'class':'form-control'}),
+            'email': forms.EmailInput(attrs={'class':'form-control'}),
+            'location': forms.TextInput(attrs={'class':'form-control'}),
+            'patient_category': forms.Select(attrs={'class':'form-select'}),
+            'details': forms.TextInput(attrs={'class':'form-control'}),
+        }
