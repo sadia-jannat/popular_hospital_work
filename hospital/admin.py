72d6229e53de4884f75bd060a7acbb12cc616771
sadia-jannat <37320382+sadia-jannat@users.noreply.github.com>
2023-06-17 12:32:38 +0600

    Update admin.py

--- hospital/admin.py
+++ hospital/admin.py
@@ -8,3 +8,9 @@ from  .models import DoctorDetails
 @admin.register(DoctorDetails)
 class DoctorDetailsAdmin(admin.ModelAdmin):
     list_display =('name','email','phone','nid','area','special','daywork')
+
+
+from  .models import Patient
+@admin.register(Patient)
+class PatientAdmin(admin.ModelAdmin):
+    list_display=('doctor_name','name','phone','email','location','patient_category','details') 
